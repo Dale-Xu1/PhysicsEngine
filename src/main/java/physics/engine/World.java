16fe3bb9fcf9abb5facf446dacd2a83df28d1186
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-09-15 17:42:47 -0400

    Added gravity and scale to world

--- src/main/java/physics/engine/World.java
+++ src/main/java/physics/engine/World.java
@@ -1,5 +1,55 @@
 package physics.engine;
 
+import physics.engine.math.Vector2;
+
 public class World
 {
+
+    private static final float GRAVITY = -9.81f;
+    private static final float SCALE = 10;
+
+    private final Vector2 gravity;
+    private final float scale;
+
+
+    public World(Vector2 gravity, float scale)
+    {
+        this.gravity = gravity;
+        this.scale = scale;
+    }
+
+    public World(Vector2 gravity)
+    {
+        this(gravity, SCALE);
+    }
+
+    public World(float scale)
+    {
+        this(new Vector2(0, GRAVITY), scale);
+    }
+
+    public World()
+    {
+        this(SCALE);
+    }
+
+
+    public Vector2 toScreen(Vector2 vector)
+    {
+        // Scale vector and invert y-axis
+        Vector2 scaled = vector.mult(scale);
+        return new Vector2(scaled.x, -scaled.y); // We want y to point up in this world
+    }
+
+    public float toScreen(float value)
+    {
+        return value * scale;
+    }
+
+
+    public Vector2 getGravity()
+    {
+        return gravity;
+    }
+
 }
