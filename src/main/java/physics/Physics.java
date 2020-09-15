6e8be851d7bec9dfa9cf3ef7233c21f242256ef4
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-09-15 17:06:14 -0400

    Created canvas

--- src/main/java/physics/Physics.java
+++ src/main/java/physics/Physics.java
@@ -1,6 +1,9 @@
 package physics;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 public class Physics extends Parent
 {
@@ -9,5 +12,43 @@ public class Physics extends Parent
 
     public static final int WIDTH = 720;
     public static final int HEIGHT = 420;
+
+
+    private class Animation extends AnimationTimer
+    {
+        @Override
+        public void handle(long now)
+        {
+            // Clear screen
+            gc.clearRect(0, 0, WIDTH, HEIGHT);
+            gc.save();
+
+            render();
+            gc.restore();
+        }
+    }
+
+    private final GraphicsContext gc;
+
+
+    public Physics()
+    {
+        // Create canvas
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        getChildren().add(canvas);
+
+        gc = canvas.getGraphicsContext2D();
+
+        // Start animation
+        Animation animation = new Animation();
+        animation.start();
+    }
+
+
+    private void render()
+    {
+        gc.translate(WIDTH / 2f, HEIGHT / 2f);
+        gc.fillRect(0, 0, 100, 100);
+    }
 
 }
