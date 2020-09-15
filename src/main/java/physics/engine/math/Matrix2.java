564f41bffcf5a256b081824993360d99ec1f098f
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-09-15 17:38:29 -0400

    Matrix and vector

---
+++ src/main/java/physics/engine/math/Matrix2.java
@@ -0,0 +1,32 @@
+package physics.engine.math;
+
+public class Matrix2
+{
+
+    public final float m00, m01;
+    public final float m10, m11;
+
+
+    public Matrix2(float angle)
+    {
+        // Create rotation matrix
+        float cos = (float) Math.cos(angle);
+        float sin = (float) Math.sin(angle);
+
+        m00 = cos;
+        m01 = -sin;
+        m10 = sin;
+        m11 = cos;
+    }
+
+
+    public Vector2 mult(Vector2 vector)
+    {
+        // Perform matrix multiplication
+        float x = (m00 * vector.x) + (m01 * vector.y);
+        float y = (m10 * vector.x) + (m11 * vector.y);
+
+        return new Vector2(x, y);
+    }
+
+}
---
+++ src/main/java/physics/engine/math/Vector2.java
@@ -0,0 +1,66 @@
+package physics.engine.math;
+
+public class Vector2
+{
+
+    public final float x;
+    public final float y;
+
+
+    public Vector2(float x, float y) // Fields are never mutated so no copying constructor is used
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public Vector2 add(Vector2 vector)
+    {
+        return new Vector2(x + vector.x, y + vector.y);
+    }
+
+    public Vector2 sub(Vector2 vector)
+    {
+        return new Vector2(x - vector.x, y - vector.y);
+    }
+
+
+    public Vector2 mult(float value)
+    {
+        return new Vector2(x * value, y * value);
+    }
+
+    public Vector2 negate()
+    {
+        return new Vector2(-x, -y);
+    }
+
+
+    public float dot(Vector2 vector)
+    {
+        return (x * vector.x) + (y * vector.y);
+    }
+
+    public float cross(Vector2 vector)
+    {
+        return (x * vector.y) - (y * vector.x);
+    }
+
+
+    public float magSq()
+    {
+        return (x * x) + (y * y);
+    }
+
+    public float mag()
+    {
+        return (float) Math.sqrt(magSq());
+    }
+
+    public Vector2 normalize()
+    {
+        float magnitude = mag();
+        return (magnitude > 0) ? mult(1 / magnitude) : this;
+    }
+
+}
